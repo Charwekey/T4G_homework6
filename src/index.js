b10d6c2c86d34d010ebfbb45e0d8694b6d83ab10
Charwekey <charwekeyrabisabutey@gmail.com>
2024-11-29 07:16:11 +1200

    Added all files

---
+++ src/index.js
@@ -0,0 +1,51 @@
+
+let account =prompt("What is ur username?") ;
+let pass= prompt("Reset password?");
+let email = prompt("Type in your email");
+let password= prompt("Type in your new password.");
+let confirm= prompt("Confirm password.");
+
+alert(`${email} you have sucessfully reset your password🎉🎉` )
+
+
+
+
+
+
+
+let name= "Charwekey";
+console.log(name);
+
+let age="19";
+console.log(age)
+
+let num1 = 13;
+let num2 = 7;
+console.log(num1 + num2);
+
+let num3 = 20;
+let num4 = 1;
+console.log(num3 - num4);
+
+let num5 = 44;
+let num6 = 2;
+console.log(num5 / num6);
+
+
+let num7 = 90;
+let num8 = 4;
+console.log(num7 * num8);
+
+let num9 = 15;
+let num10 = 4;
+console.log(num9 % num10);
+
+let rain = true;
+console.log(rain);
+
+let yam = 12;
+console.log(typeof(yam));
+
+let baby = "Ayan"
+console.log(typeof(baby));
+
